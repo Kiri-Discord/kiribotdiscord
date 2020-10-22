d9769fedad7b96856f9c0e575566370cea07501f
ryzenix <61915235+ryzenix@users.noreply.github.com>
2020-10-22 20:19:20 +0700

    i dont know what to do with my life anymore

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
-const config = require('./config.json');
+const { prefix } = require('./config.json');
 
 client.once('ready', () => {
     console.log(`i am ready!`);
