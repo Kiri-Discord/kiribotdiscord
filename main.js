eb8a5f670758c9647aa6ffd2e7d72413abb634cc
ryzenix <61915235+ryzenix@users.noreply.github.com>
2020-10-22 20:17:16 +0700

    fix ping command

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
-const prefix = require('./config.json');
+const config = require('./config.json');
 
 client.once('ready', () => {
     console.log(`i am ready!`);
