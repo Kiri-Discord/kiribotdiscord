510af7851a09aa7d90152f336eec93f14656627c
ryzenix <61915235+ryzenix@users.noreply.github.com>
2020-10-22 20:13:30 +0700

    Fix minor coding error

--- main.js
+++ main.js
@@ -8,9 +8,9 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-    if(!message.content.startsWith(prefix) | | message.author.bot) return;
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
     const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();\
+    const command = args.shift().toLowerCase();
     if(command --- 'ping'){
         message.channel.send('pong!');
     }
