b771de3bcdb7f2b0d82922e6c5cbcbf22405bcd8
ryzenix <61915235+ryzenix@users.noreply.github.com>
2020-10-22 19:04:26 +0700

    Hidden the token
    thats it

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ const client = new Discord.Client();
 
 
 client.once('ready', () => {
-    console.log('i am ready!');
+    console.log(`i am ready!`);
 });
 
 
 
-client.login('NzY4NDY4Mjk3NTUyNzU2Nzk3.X5A53w.5WKj3-tYDuhidRqOP5OVpOg2r6Y');
+client.login('process.env.token');
