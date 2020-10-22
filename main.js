86fb3f8c3bef190cd896b9a5a664bac647ebebdd
ryzenix <61915235+ryzenix@users.noreply.github.com>
2020-10-22 20:06:41 +0700

    Added ping command

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
-
+const prefix = require('./config.json');
 
 client.once('ready', () => {
     console.log(`i am ready!`);
 });
 
-
-
+client.on('message', message => {
+    if(!message.content.startsWith(prefix) | | message.author.bot) return;
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();\
+    if(command --- 'ping'){
+        message.channel.send('pong!');
+    }
+});
 client.login(process.env.token);
