3492422c9594e24f7f39b970a214744e25151e72
ryzenix <61915235+ryzenix@users.noreply.github.com>
2020-10-22 11:43:05 +0700

    update 1

---
+++ main.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+
+client.once('ready', () => {
+    console.log('i am ready!');
+});
+
+
+
+client.login('NzY4NDY4Mjk3NTUyNzU2Nzk3.X5A53w.5WKj3-tYDuhidRqOP5OVpOg2r6Y');
