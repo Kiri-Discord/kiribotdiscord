bd050400e9cb89b60c59b217c6dd447a87421b76
ryzenix <61915235+ryzenix@users.noreply.github.com>
2020-10-23 11:31:18 +0700

    FIX PING AGAIN
    fuck you

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ client.once('ready', () => {
     console.log(`i am ready!`);
 });
 
-client.on('message', message => {
+client.on('message', message =>{
     if(!message.content.startsWith(prefix) || message.author.bot) return;
     const args = message.content.slice(prefix.length).split(/ +/);
 
