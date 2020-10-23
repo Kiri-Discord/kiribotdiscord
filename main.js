ed84619e24fe26347cac1ae56a4acae4598247bd
ryzenix <61915235+ryzenix@users.noreply.github.com>
2020-10-23 11:19:14 +0700

    Fix ping

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
-const prefix = "/";
+const prefix = '/';
 
 client.once('ready', () => {
     console.log(`i am ready!`);
@@ -15,6 +15,6 @@ client.on('message', message => {
     
     if(command --- 'ping'){
         message.channel.send('pong!');
-    };
+    }
 });
 client.login(process.env.token);
