d11bf6b7b0c44410eade7dc524e75a99e1b403b6
ryzenix <61915235+ryzenix@users.noreply.github.com>
2020-10-23 11:01:38 +0700

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,9 @@ client.once('ready', () => {
 client.on('message', message => {
     if(!message.content.startsWith(prefix) || message.author.bot) return;
     const args = message.content.slice(prefix.length).split(/ +/);
+
     const command = args.shift().toLowerCase();
+    
     if(command --- 'ping'){
         message.channel.send('pong!');
     }
