9690dba8073204be798a76260fb810597dbf836c
ryzenix <61915235+ryzenix@users.noreply.github.com>
2020-10-25 15:31:31 +0700

    Fix image thumbnail help

--- commands/help.js
+++ commands/help.js
@@ -12,7 +12,7 @@ module.exports = {
       .setDescription("list of all commands (for now)")
       .setColor("#F8AA2A")
       .setFooter("this is a wip bot. image credit: azurie「リン」#9528")
-      .setThumbnail('https://images-ext-2.discordapp.net/external/9U04vbD9GLf31Hhxj66zbsG3JWELA_mzbTyL0TnVFNg/https/i.imgur.com/S2AOUgx.png?width=389&height=278');
+      .setImage('https://images-ext-2.discordapp.net/external/9U04vbD9GLf31Hhxj66zbsG3JWELA_mzbTyL0TnVFNg/https/i.imgur.com/S2AOUgx.png?width=389&height=278');
 
     commands.forEach((cmd) => {
       helpEmbed.addField(
