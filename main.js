258ec623dfb0a2df105c55dd8ab2cc7cca09c30d
ryzenix <61915235+ryzenix@users.noreply.github.com>
2020-10-30 19:25:13 +0700

    fix token bug

--- main.js
+++ main.js
@@ -8,4 +8,4 @@ require("./handler/Event.js")(client);
 client.package = require("./package.json");
 client.on("warn", console.warn); // This will warn you via logs if there was something wrong with your bot.
 client.on("error", console.error); // This will send you an error message via logs if there was something missing with your coding.
-client.login(process.env.SECRET).catch(console.error); // This token will leads to the .env file. It's safe in there.
+client.login(process.env.token).catch(console.error); // This token will leads to the .env file. It's safe in there.
