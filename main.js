b3420a597388d1608bd7177b1acd72a23cd0cc64
ryzenix <61915235+ryzenix@users.noreply.github.com>
2020-10-30 20:17:54 +0700

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const client = new sefy();
 require("./handler/module.js")(client);
 require("./handler/Event.js")(client);
 
-client.package = require("./package.json");
+
 client.on("warn", console.warn); // This will warn you via logs if there was something wrong with your bot.
 client.on("error", console.error); // This will send you an error message via logs if there was something missing with your coding.
 client.login(process.env.token).catch(console.error); // This token will leads to the .env file. It's safe in there.
