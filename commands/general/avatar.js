51ad4a7774274c8354ff13200d0dae2f2220d588
ryzenix <61915235+ryzenix@users.noreply.github.com>
2020-11-01 21:40:11 +0700

    Update avatar.js

--- commands/general/avatar.js
+++ commands/general/avatar.js
@@ -19,7 +19,7 @@ exports.run = async (client, message, args) => {
   const embed = new Discord.MessageEmbed()
   .setTitle(`${user.tag} avatar`)
   .setDescription(`[Avatar URL of **${user.tag}**](${avatar})`)
-  .setColor(0x1d1d1d)
+  .setColor(DAF7A6)
   .setImage(avatar)
   
   return message.channel.send(embed);
