992337efb1a826e9e913335288ff999a9b73e82d
ryzenix <61915235+ryzenix@users.noreply.github.com>
2020-11-02 12:06:14 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,4 @@
 module.exports = client => {
-  console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setPresence({
-      status: "online", 
-      game: {
-          name: "so call me maybe",  
-          type: "LISTENING" 
-      }
-  });
-};
+  console.log(`Logged in as ${client.user.tag}`);
+  client.user.setActivity('so call me maybe', { type: 'WATCHING'}).catch(console.error);
+}
