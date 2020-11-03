71b872c454d5065dd5a0a9ac9f1f97e07e01eee1
ryzenix <61915235+ryzenix@users.noreply.github.com>
2020-11-03 22:41:19 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 const Discord = require("discord.js");
-const Constants = require('discord.js/src/util/Constants.js')
-Constants.DefaultOptions.ws.properties.$browser = `Discord iOS`
 const sefy = require("./handler/ClientBuilder.js"); // We're gonna create this soon.
 const client = new sefy();
-
+const Constants = require('discord.js/src/util/Constants.js')
+Constants.DefaultOptions.ws.properties.$browser = `Discord iOS`
 require("./handler/module.js")(client);
 require("./handler/Event.js")(client);
 
