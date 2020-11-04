77fbe916f5a5737904919c5c47e037382da971c1
ryzenix <61915235+ryzenix@users.noreply.github.com>
2020-11-04 11:34:21 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,7 @@
-
+const Constants = require('discord.js/src/util/Constants.js')
+Constants.DefaultOptions.ws.properties.$browser = `Discord iOS` 
 
 module.exports = client => {
   console.log(`Logged in as ${client.user.tag}`);
-  client.user.setActivity('so call me maybe', { type: 'WATCHING'}).catch(console.error);
+  client.user.setActivity('so call me maybe', { type: 'WATCHING', browser: "DISCORD IOS" }).catch(console.error);
 }
