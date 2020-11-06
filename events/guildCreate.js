71c05d292585482e76ade37e6d231cd0dcb33d01
ryzenix <61915235+ryzenix@users.noreply.github.com>
2020-11-06 21:55:37 +0700

    Delete guildCreate.js

--- events/guildCreate.js
+++
@@ -1,6 +0,0 @@
-module.exports = client => {
-    guild.owner.send(`Thanks for adding me to your server ${guild.name}\nMy default prefix is ` + '`>`' + `\nType >help to see a full list of commands. Have fun!`);
-    console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-    client.users.cache.get('768448397786349578').send(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-    client.users.cache.get('733281923144613918').send(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-};
