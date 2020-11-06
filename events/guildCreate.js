97d49539238b014c2b927268b7f5ce1b0107628b
ryzenix <61915235+ryzenix@users.noreply.github.com>
2020-11-06 21:54:16 +0700

    hmm

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,10 +1,3 @@
-<<<<<<< Updated upstream
-module.exports = client => {
-    guild.owner.send(`Thanks for adding me to your server ${guild.name}\nMy default prefix is ` + '`>`' + `\nType >help to see a full list of commands. Have fun!`);
-    console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-    client.users.cache.get('768448397786349578').send(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-    client.users.cache.get('733281923144613918').send(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-=======
 const Discord = require('discord.js');
 
 
@@ -41,5 +34,4 @@ module.exports = async (client, guild) => {
 
 
 
->>>>>>> Stashed changes
 };
