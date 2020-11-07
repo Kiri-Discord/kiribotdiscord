a268d29808f57c05921ca147d208d70015e83e37
ryzenix <61915235+ryzenix@users.noreply.github.com>
2020-11-07 20:55:19 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,4 @@
 
-
 module.exports = client => {
   console.log(`Logged in as ${client.user.tag}`);
   client.user.setActivity('over you all', { type: 'WATCHING', browser: "DISCORD IOS" }).catch(console.error);
