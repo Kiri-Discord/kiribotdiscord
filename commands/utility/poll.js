8afc552cb71ba3ec154c4456af763fc4bc635fde
ryzenix <61915235+ryzenix@users.noreply.github.com>
2020-11-09 11:29:26 +0700

    Create poll.js

---
+++ commands/utility/poll.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+
+exports.run = async (client, message, args) => {
+        let pollChannel = message.mentions.channels.first();
+        if (!pollChannel) return message.channel.send("Please mention a channel!");
+
+        let pollDescription = args.slice(1).join(' ');
+
+        let embedPoll = new Discord.MessageEmbed()
+        .setTitle('New poll! ❗ ')
+        .setDescription(pollDescription)
+        .setColor('YELLOW')
+        let msgEmbed = await pollChannel.send(embedPoll);
+        await msgEmbed.react('👍')
+        await msgEmbed.react('👎')
+    }
+
+exports.help = {
+        name: "poll",
+    description: "Create a poll in an defined channel",
+    usage: "poll <topic>",
+    example: "poll yes or no"
+}
+
+exports.conf = {
+    aliases: [""],
+    cooldown: 30
+}
+ 
