f6fd1819df8a5626ab1567f382710bdfe392c850
ryzenix <61915235+ryzenix@users.noreply.github.com>
2020-11-10 23:02:45 +0700

    Update trump.js

--- commands/image/trump.js
+++ commands/image/trump.js
@@ -2,7 +2,6 @@ const jimp = require("jimp")
 
 exports.run = async (client, message, args) => {
 
-    message.channel.startTyping();
 
     let img = jimp.read("https://i.imgur.com/sLaJt9q.png")
     if (!args[0]) return message.reply("Indicate that Trump should speak pls :D")
@@ -12,7 +11,6 @@ exports.run = async (client, message, args) => {
             image.print(font, 22, 120, args.join(" "), 600)
             image.getBuffer(jimp.MIME_PNG, (err, i) => {
                 message.channel.send({files: [{ attachment: i, name: "trump.png"}]})
-                message.channel.stopTyping();
             })
         })
     })
