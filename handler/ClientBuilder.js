74419da0c9c5e3c71dabc9ff50e867d31350f8f7
ryzenix <61915235+ryzenix@users.noreply.github.com>
2020-11-18 21:33:13 +0700

    Update ClientBuilder.js

--- handler/ClientBuilder.js
+++ handler/ClientBuilder.js
@@ -1,4 +1,4 @@
-const {Client, Collection, Guild, GuildMember} = require("discord.js");
+const {Client, Collection, Guild, GuildMember, Structures} = require("discord.js");
 const { readdir, readdirSync } = require('fs');
 const { join, resolve } = require('path');
 
