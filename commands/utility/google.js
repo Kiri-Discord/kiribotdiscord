21714c3ac18fb403868178ed5a78fe9cadfc8580
ryzenix <61915235+ryzenix@users.noreply.github.com>
2020-11-20 10:00:59 +0700

    added youtube

--- commands/utility/google.js
+++ commands/utility/google.js
@@ -8,10 +8,10 @@ exports.run = async (client, message, args) => {
     let query = args.join(" ");
     let result;
 
-    if (!query) return message.channel.send("Enter something so i can search 👀");
+    if (!query) return message.channel.send("enter something so i can search 👀");
 
     href = await search(query);
-    if (!href) return message.channel.send("Unknown search.");
+    if (!href) return message.channel.send("unknown search :(");
 
     const embed = new Discord.MessageEmbed()
     .setTimestamp(new Date())
@@ -19,9 +19,8 @@ exports.run = async (client, message, args) => {
     .setDescription(href.snippet)
     .setImage(href.pagemap ? href.pagemap.cse_thumbnail[0].src : null) // Sometimes, the thumbnail might be unavailable in variant site. Return it to null.
     .setURL(href.link)
-    .setColor('#DAF7A6')
+    .setColor('RANDOM')
     .setAuthor('Google', 'https://i.pinimg.com/originals/74/65/f3/7465f30319191e2729668875e7a557f2.png')
-
     .setFooter(message.member.displayName,  message.author.displayAvatarURL({ dynamic: true }))
     return message.channel.send(embed).catch(err => message.channel.send("i can't seem to be able to grab you a result for that :( here is a hug for now 🤗"));
 
