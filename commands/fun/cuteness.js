7aab37d0757a83e86a1cd771b978003458a1b14f
ryzenix <61915235+ryzenix@users.noreply.github.com>
2020-12-08 00:38:06 +0700

    Update cuteness.js

--- commands/fun/cuteness.js
+++ commands/fun/cuteness.js
@@ -4,8 +4,9 @@ const texts = require('../../assets/cuteness');
 exports.run = async (client, message, args) => {
     const user = message.mentions.users.first() || message.author;
     const authorUser = user.id === message.author.id;
-    if (user.bot) return message.reply('have you ever watched Ex Machina?');
     if (user.id === client.user.id) return message.channel.send('me? well you decide instead :D');
+    if (user.bot) return message.reply('have you ever watched Ex Machina?');
+    
     const random = MersenneTwister19937.seed(user.id);
     const cuteness = integer(0, texts.length - 1)(random);
     return message.channel.send(`i think ${authorUser ? 'you are' : `${user.username} is`} ${texts[cuteness]}`);
