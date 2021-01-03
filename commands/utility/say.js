97f72f17125811a956e5e2a70b56fa1de2d313e3
ryzenix <61915235+ryzenix@users.noreply.github.com>
2021-01-03 17:17:26 +0700

    fixed more bugs

--- commands/utility/say.js
+++ commands/utility/say.js
@@ -29,12 +29,12 @@ exports.run = async (client, message, args) => {
   if (!languages.includes(act)) return message.reply(`i don't recognize that language :( use \`${prefix}help say\` to show the list of langauge that i can speak!`)
   if (!toMp3) message.reply(`you must tell me something to say! use \`${prefix}help say\` to show the usage for this one :)`);
   if (toMp3.length > 200) message.reply(`your text is longer than 200 words, which makes me harder to upload my record :( im still saying it btw`);
-  const current = client.voicequeue.get(`${message.author.id}-${message.guild.id}`);
+  const current = client.voicequeue.get(message.guild.id);
 	if (current) return message.reply(current.prompt);
 
     try {
       if (message.member.voice.channel) {
-        await client.voicequeue.set(`${message.author.id}-${message.guild.id}`, { prompt: `please wait until i finish talking to **${message.author.username}** :(\n *i can only talk in one server at a time, in one server. just like real people*` });
+        await client.voicequeue.set(message.guild.id, { prompt: `please wait until i finish talking to **${message.author.username}** :(\n *i can only talk in one server at a time, in one server. just like real people*` });
         const link = await tts.getAllAudioUrls(toMp3, {
           lang: act,
           slow: false,
@@ -47,7 +47,7 @@ exports.run = async (client, message, args) => {
           msg = await message.channel.send('hey i started talking')
         });        
         dispatcher.on('finish', async () => {
-          await client.voicequeue.delete(`${message.author.id}-${message.guild.id}`);
+          await client.voicequeue.delete(message.guild.id);
           await connection.disconnect();
           return msg.edit('done')
         });
@@ -55,7 +55,7 @@ exports.run = async (client, message, args) => {
         return message.channel.send(`you have to join a voice channel first!`)
       }
     } catch {
-      await client.voicequeue.delete(`${message.author.id}-${message.guild.id}`);
+      await client.voicequeue.delete(message.guild.id);
       return message.reply('sorry, i got an error while sending you my voice record :( try again later!') 
     }
 }
