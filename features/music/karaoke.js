7566d29cb82d1a33b61b15d8c0407a76dd29b6c1
ryzenix <61915235+ryzenix@users.noreply.github.com>
2021-03-01 10:48:00 +0700

    make lyric from internet always display when there isnt any caption

--- features/music/karaoke.js
+++ features/music/karaoke.js
@@ -11,12 +11,10 @@ module.exports = {
     async sing(song, message, channel, lang, queue) {
         if (song.type === 'yt') {
             const info = await ytdl.getInfo(song.url);
-            const tracks = info
-            .player_response.captions
-            .playerCaptionsTracklistRenderer.captionTracks;
-
-    
-            if (tracks && tracks.length) {
+            const foundCaption = info.player_response.captions;
+            if (foundCaption) {
+              const tracks = foundCaption.playerCaptionsTracklistRenderer.captionTracks;
+              if (tracks && tracks.length) {
                 const track = tracks.find(t => t.languageCode === lang);
                 if (track) {
                   const { body } = await request
@@ -39,28 +37,39 @@ module.exports = {
                   });
                 } else {
                     const lyrics = await lyricsFinder(song.title, '');
-                    if (!lyrics) return message.reply({embed: {color: "f3f3f3", description: `i found no lyrics for **${song.title}** by **${song.author}** in your language \`${ISO6391.getName(lang)}\` :pensive:\nyou can try using an another language.\n\n*don't know what is this about? auto-scroll lyrics mode is currently set to \`ON\` in your guild setting :)*`}});
+                    if (!lyrics) return message.channel.send({embed: {color: "f3f3f3", description: `${song.requestedby}, i found no lyrics for **${song.title}** by **${song.author}** in your language \`${ISO6391.getName(lang)}\` :pensive:\nyou can try using an another language.`}});
                     let lyricsEmbed = new MessageEmbed()
                     .setTitle(`Lyrics for ${song.title} by ${song.author}`)
                     .setDescription(lyrics)
                     .setColor('RANDOM')
                     if (lyricsEmbed.description.length >= 2048)
                     lyricsEmbed.description = `${lyricsEmbed.description.substr(0, 2045)}...`;
-                    return channel.send(`${song.requestedby} sorry, there isn\'t any lyric language of **${song.title}** found in your language \`${ISO6391.getName(lang)}\` :pensive: showing you the lyric that i found on internet instead...\n*don't know what is this about? auto-scroll lyrics mode is currently set to \`ON\` in your guild setting :)*`, lyricsEmbed);
+                    return channel.send(`${song.requestedby}, i found some lyrics of **${song.title}**, but not in your language \`${ISO6391.getName(lang)}\` :pensive:\nshowing you the lyric that i found on internet instead...\nyou can try using an another language.`, lyricsEmbed);
                 }
               } else {
                 const lyrics = await lyricsFinder(song.title, '');
-                if (!lyrics) return message.reply({embed: {color: "f3f3f3", description: `i found no lyrics for **${song.title}** by **${song.author}** in your language \`${ISO6391.getName(lang)} :pensive:\nmay be the link you requested from YouTube isn't a song?\n*don't know what is this about? auto-scroll lyrics mode is currently set to \`ON\` in your guild setting :)*`}});
+                if (!lyrics) return message.channel.send({embed: {color: "f3f3f3", description: `${song.requestedby}, i found no lyrics for **${song.title}** by **${song.author}** in your language \`${ISO6391.getName(lang)}\` :pensive:\nyou can try using an another language.`}});
                 let lyricsEmbed = new MessageEmbed()
                 .setTitle(`Lyrics for ${song.title} by ${song.author}`)
                 .setDescription(lyrics)
                 .setColor('RANDOM')
                 if (lyricsEmbed.description.length >= 2048)
                 lyricsEmbed.description = `${lyricsEmbed.description.substr(0, 2045)}...`;
-                return channel.send(`${song.requestedby} sorry, i can't find any lyric of **${song.title}** by **${song.author}** in your language \`${ISO6391.getName(lang)}\`\n*showing you the lyric that i found on internet instead...\n\n*don't know what is this about? auto-scroll lyrics mode is currently set to \`ON\` in your guild setting :)*`, lyricsEmbed);
+                return channel.send(`${song.requestedby}, i found some lyrics of **${song.title}**, but not in your language \`${ISO6391.getName(lang)}\` :pensive:\n*showing you the lyric that i found on internet instead...`, lyricsEmbed);
               }
+            } else {
+              const lyrics = await lyricsFinder(song.title, '');
+              if (!lyrics) return message.channel.send({embed: {color: "f3f3f3", description: `${song.requestedby}, i found no lyrics for **${song.title}** by **${song.author}** :pensive:\n\n*don't know what is this about? auto-scroll lyrics mode is currently set to \`ON\` in your guild setting :)*`}});
+              let lyricsEmbed = new MessageEmbed()
+              .setTitle(`Lyrics for ${song.title} by ${song.author}`)
+              .setDescription(lyrics)
+              .setColor('RANDOM')
+              if (lyricsEmbed.description.length >= 2048)
+              lyricsEmbed.description = `${lyricsEmbed.description.substr(0, 2045)}...`;
+              return channel.send(`${song.requestedby}, i found some lyrics of **${song.title}**, but not in your language \`${ISO6391.getName(lang)}\` :pensive:\n*showing you the lyric that i found on internet instead...`, lyricsEmbed);
+            }
         } else {
-          return channel.send({embed: {color: "f3f3f3", description: `i'm sorry but auto-scroll lyrics mode doesn't work yet with SoundCloud track :pensive:\n\n*don't know what is this about? karaoke mode is currently set to \`ON\` in your guild setting :)*`}});
+          return channel.send({embed: {color: "f3f3f3", description: `i'm sorry but auto-scroll lyrics mode doesn't work yet with SoundCloud track :pensive:*`}});
         }
     }
 }
