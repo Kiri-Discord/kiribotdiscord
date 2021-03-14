964eaa02f91b80526242412a47660b5454cd5761
ryzenix <61915235+ryzenix@users.noreply.github.com>
2021-03-14 14:02:06 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,7 @@ module.exports = {
   apps : [{
     script: 'main.js',
     name: 'Sefy',
-    cwd: '/home/sefy/actions-runner/sefybotdiscord/sefybotdiscord/sefybotdiscord/'
+    cwd: '/home/sefy/actions-runner/sefybotdiscord/sefybotdiscord/sefybotdiscord/',
+    node_args : '-r dotenv/config',
   }],
 };
