e8c2b48f4757a382b8d175f2c327329c80111c25
ryzenix <61915235+ryzenix@users.noreply.github.com>
2021-03-16 22:18:40 +0700

    fix 404 page entirely (#42)
    * trying to add 404 page
    
    * add rick roll
    
    * .
    
    * Update main.js

--- util/web.js
+++ util/web.js
@@ -6,7 +6,7 @@ module.exports = {
         client.webapp.use(`/assets`, express.static(__basedir + '/html/assets/'));
         client.webapp.get('/', (_, res) => res.sendFile(__basedir + '/html/landing.html'));
         client.webapp.get('*', function(req, res) {
-            res.status(404);
+            res.sendFile(__basedir + '/html/404.html')
         });
         client.webapp.listen(_port);
         console.log(`[WEB] Listening at port ${_port}`);
