8ef2fe620e95a862a16757a3fab6ff296ac397f7
ryzenix <61915235+ryzenix@users.noreply.github.com>
2021-03-16 17:47:23 +0700

    add rick roll

--- util/web.js
+++ util/web.js
@@ -5,12 +5,8 @@ module.exports = {
         client.webapp.use(express.json());
         client.webapp.use(`/assets`, express.static(__basedir + '/html/assets/'));
         client.webapp.get('/', (_, res) => res.sendFile(__basedir + '/html/landing.html'));
-        client.webapp.get('/404', function(req, res) {
-            res.sendFile(__basedir + '/html/404.html');
-        });
         client.webapp.get('*', function(req, res) {
-            res.status(301).redirect(`${__baseURL}404`);
-            res.end();
+            res.sendFile(__basedir + '/html/404.html')
         });
         client.webapp.listen(_port);
         console.log(`[WEB] Listening at port ${_port}`);
