333159e14c6ed06393922eb7ad0d6b3012faf0e3
ryzenix <61915235+ryzenix@users.noreply.github.com>
2021-03-16 07:17:46 +0700

    fix verify

--- model/verify.js
+++ model/verify.js
@@ -4,7 +4,7 @@ const verifySchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     guildID: String,
     userID: String,
-    valID: String
+    code: String
 });
 
 module.exports = mongoose.model('verify', verifySchema, 'verify');
