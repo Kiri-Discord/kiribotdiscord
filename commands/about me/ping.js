6193f71f6dde9adcb53e872e34a5b24b3933bea5
ryzenix <61915235+ryzenix@users.noreply.github.com>
2021-04-10 12:08:29 +0700

    Update ping.js

--- commands/about me/ping.js
+++ commands/about me/ping.js
@@ -1,5 +1,5 @@
 exports.run = async (client, message, args) => {
-	return message.channel.send({embed: {color: "f3f3f3", description: `🏓 **pong!** took me roughly **${- (Date.now() - message.createdTimestamp)}ms** to hit back, and the Discord API has a latency of **${Math.round(client.ws.ping)}ms**!`}});
+	return message.channel.send({embed: {color: "f3f3f3", description: `🏓 **pong!** took me roughly **${(Date.now() - message.createdTimestamp)}ms** to hit back, and the Discord API has a latency of **${Math.round(client.ws.ping)}ms**!`}});
 };
 exports.help = {
 	name: "ping",
