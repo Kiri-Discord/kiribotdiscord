ecf8c289ea9f32118ef4ab98438eb035bcd94992
ryzenix <61915235+ryzenix@users.noreply.github.com>
2021-04-10 12:10:02 +0700

    Update beep.js

--- commands/about me/beep.js
+++ commands/about me/beep.js
@@ -1,6 +1,6 @@
 exports.run = async (client, message, args) => {
   const blessEmoji = client.customEmojis.get('bless') ? client.customEmojis.get('bless') : '✔️' ;
-	return message.channel.send({embed: {color: "f3f3f3", description: `${blessEmoji} **boop!** took me **${- (Date.now() - message.createdTimestamp)}ms** to reply, and the Discord API has a latency of **${Math.round(client.ws.ping)}ms**!`}});
+	return message.channel.send({embed: {color: "f3f3f3", description: `${blessEmoji} **boop!** took me **${(Date.now() - message.createdTimestamp)}ms** to reply, and the Discord API has a latency of **${Math.round(client.ws.ping)}ms**!`}});
 };
 
 exports.help = {
