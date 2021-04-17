1f56bd330def8f6c26940218844ca96157ae76a1
ryzenix <61915235+ryzenix@users.noreply.github.com>
2021-04-18 00:38:53 +0700

    my stupid mistake

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -21,10 +21,12 @@ module.exports = async (client, guild) => {
 
 
   const prefix = client.config.prefix;
+  const blush = client.customEmojis.get('blush') ? client.customEmojis.get('blush') : ':blush:';
+  const duh = client.customEmojis.get('duh') ? client.customEmojis.get('duh') : ':blush:';
 
   const embed = new MessageEmbed()
   .setTitle('Thanks for inviting me :D')
-  .setDescription(`Thanks for adding me to your server **${guild.name}** ^^\nMy default prefix is ` + prefix + `\n\nType \`${prefix}help\` to get started! Have fun!`)
+  .setDescription(`Thanks for adding me to your server **${guild.name}** ${duh}\nMy default prefix is \`${prefix}\`\n\nType \`${prefix}help\` to get started! Have fun!`)
   .setColor('#DAF7A6')
   .setTimestamp(new Date())
   .setAuthor(client.user.username, client.user.displayAvatarURL())
@@ -35,7 +37,7 @@ module.exports = async (client, guild) => {
   if (log1) log1.send(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
   const log2 = client.channels.cache.get('774476096409436170')
   if (log2) log2.send(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-    
+
   await guild.owner.send(embed);
   guild.owner.send(`**some of my suggestion for you to get started:** ${blush}\n\n\`${prefix}invite\` - check out our community server and support server.\n\`${prefix}help\` - my command list\n\`${prefix}levelings\` and \`${prefix}levelingignore\` - set up levelings, and set a channel to ignore messages from leveling up\n\`${prefix}invite\` - set your server's own verification portal, powered by Google reCAPTCHA.\n\n*and many more to come...*`)
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
