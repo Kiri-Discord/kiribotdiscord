ef5ccef66f404a468afbca7b6c6fb3794ce0514b
ryzenix <61915235+ryzenix@users.noreply.github.com>
2021-04-22 19:15:30 +0700

    code --> link

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -45,7 +45,7 @@ module.exports = async (client, member) => {
     .setDescription(`Hello! Before you join ${member.guild.name}, I just want you to verify yourself first. Enter the link below and solve the captcha to verify yourself. Hurry up, if you don't verify fast you will be kicked from the server in \`${ms(timeMs, {long: true})}\` to prevent bots and spams :pensive:`)
     .addField(`Verification link for ${member.user.username}`, `||${__baseURL}verify?valID=${code}||`)
     await member.send(dm).catch(() => {
-      verifyChannel.send(`<@!${member.user.id}> uh, your DM is locked so i can't send you the verify code. can you unlock it first and type \`resend\` here?`)
+      verifyChannel.send(`<@!${member.user.id}> uh, your DM is locked so i can't send you the verify link. can you unlock it first and type \`resend\` here?`)
       .then(i => i.delete({timeout: 10000}));
     })
   }
