a5ad37b52c972253b03e9a2c39f2a0578e2cff34
ryzenix <61915235+ryzenix@users.noreply.github.com>
2021-04-29 21:32:54 +0700

    prepare for the last part

--- util/web.js
+++ util/web.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const helmet = require("helmet");
 
 module.exports = {
     init: (client) => {
         client.webapp.use(express.json());
-        client.webapp.get('/', (_, res) => res.send('the heck bros'));
+        client.webapp.use(helmet());
+        client.webapp.get('/', authenticateToken, (_, res) => res.send('the heck bros'));
         client.webapp.post("/checkVerify", authenticateToken, async (req, res) => {
             if ("userID" in req.query && "guildID" in req.query) {
+              await client.dbverify.findOneAndDelete({
+                guildID: req.query.userID,
+                userID: req.query.guildID
+              });
+              await client.verifytimers.deleteTimer(req.query.guildID, req.query.userID);
               const setting = await client.dbguilds.findOne({
                 guildID: req.query.guildID
               });
@@ -19,40 +26,36 @@ module.exports = {
               if (!guild) return res.json({ code: 204, message: 'NO_GUILD_FOUND' })
               if (!guild.available) return res.json({ code: 204, message: 'GUILD_NOT_AVAILABLE' });
               if (guild.partial) await guild.fetch();
-              const member = guild.members.cache.get(req.query.guildID);
+              const member = guild.members.cache.get(req.query.userID);
               if (!member) return res.json({ code: 204, message: 'MEMBER_NOT_FOUND' });
               if (member.partial) await member.fetch();
               const roleExist = member._roles.includes(setting.verifyRole);
               if (roleExist) return res.json({ code: 204, message: 'ALREADY_VERIFIED' });
+              const verifyRole = guild.roles.cache.get(setting.verifyRole);
               if (!verifyRole) return res.json({ code: 204, message: 'ROLE_NOT_EXIST' });
               res.json({ code: 200, message: 'SUCCESS' });
-              await client.dbverify.findOneAndDelete({
-                guildID: req.query.userID,
-                userID: req.query.guildID
-              });
-              await client.verifytimers.deleteTimer(req.query.guildID, req.query.userID);
               if (!guild.me.hasPermission('MANAGE_ROLES')) {
                 try {
                   return member.send(`oof, so mods from **${guild.name}** forgot to give me the role \`MANAGE_ROLES\` to gain you access to the server :pensive: can you ask them to verify you instead?\nyou will not be kicked after this message`);
                 } catch (error) {
                   const verifyChannel = guild.channels.cache.get(setting.verifyChannelID);
                   if (!verifyChannel || !verifyChannel.permissionsFor(guild.me).has('SEND_MESSAGES')) return;
-                  else return verifyChannel.send(`<@!${member.user.id}>, sorry but mods from **${guild.name}** forgot to give me the role \`MANAGE_ROLES\` to gain you access to the server :pensive: can you ask them to verify you instead?\nyou will not be kicked after this message`);
+                  else return verifyChannel.send(`<@!${member.user.id}>, sorry but mods from **${guild.name}** forgot to give me the role \`MANAGE_ROLES\` to gain you access to the server :pensive: can you ask them to verify you instead?\nyou will not be kicked after this message`).then(m => m.delete({ timeout: 6000 }));
                 }
               } else {
-                await member.roles.add(VerifyRole);
+                await member.roles.add(setting.verifyRole);
                 try {
                   return member.send(`**${member.user.username}**, you have passed my verification! Welcome to ${guild.name}!`);
                 } catch (error) {
                   if (!verifyChannel || !verifyChannel.permissionsFor(guild.me).has('SEND_MESSAGES')) return;
-                  else return verifyChannel.send(`<@!${member.user.id}>, you have passed my verification! Welcome to ${guild.name}!`)
+                  else return verifyChannel.send(`<@!${member.user.id}>, you have passed my verification! Welcome to ${guild.name}!`).then(m => m.delete({ timeout: 6000 }));
                 }
               }
             } else {
               return res.status(400).json({ code: 400, message: 'MISSING_QUERY' })
             }
         });
-        client.webapp.get('*', function(req, res) {
+        client.webapp.get('*', authenticateToken, function(req, res) {
             res.status(404).send('wrong call bruh')
         });
         client.webapp.listen(_port);
@@ -63,7 +66,6 @@ function authenticateToken(req, res, next) {
   const authHeader = req.headers['authorization']
   const token = authHeader && authHeader.split(' ')[1]
   if (token == null) return res.sendStatus(401);
-  if (token !== process.env.APItoken) return res.sendStatus(401);
-  if (err) return res.sendStatus(403)
+  if (token !== process.env.apiToken) return res.sendStatus(401);
   next()
 };
