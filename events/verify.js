5617427ade3c9abc0209e08920e3aea638af78f6
ryzenix <61915235+ryzenix@users.noreply.github.com>
2021-05-02 20:30:20 +0700

    Update verify.js

--- events/verify.js
+++ events/verify.js
@@ -18,10 +18,10 @@ module.exports = async (client, message) => {
 			.setDescription(`Hello! Before you join ${message.guild.name}, I just want you to verify yourself first. Enter the link below and solve the captcha to verify yourself. Hurry up, if you don't verify fast you will be kicked from the server to prevent bots and spams :pensive:*`)
 			.addField(`Verification link for ${message.author.username}`, `||${__baseURL}verify?valID=${valID}||`)
 			await message.author.send(dm).catch(() => {
-				return message.inlineReply('your DM is still locked. unlock your DM first then type \`resend\` here :D')
+				return message.channel.send('your DM is still locked. unlock your DM first then type \`resend\` here :D')
 					.then(i => i.delete({ timeout: 10000 }));
 			});
-			return message.inlineReply('check your DM :grin:').then(i => i.delete({ timeout: 10000 }));
+			return message.channel.send('check your DM :grin:').then(i => i.delete({ timeout: 10000 }));
 		} else {
 			if (message.channel.permissionsFor(message.guild.me).has('MANAGE_MESSAGES')) return message.delete();
 		}
