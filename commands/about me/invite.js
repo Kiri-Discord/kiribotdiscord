c398ed6bf7cf66411ba309cda9b2d8013daad1c6
ryzenix <61915235+ryzenix@users.noreply.github.com>
2021-06-17 16:58:54 +0700

    change the invite link

--- commands/about me/invite.js
+++ commands/about me/invite.js
@@ -4,7 +4,7 @@ exports.run = async (client, message, args) => {
   const embed = new MessageEmbed()
   .setThumbnail(client.user.displayAvatarURL({size: 4096, dynamic: true}))
   .setColor('#ffe6cc')
-  .addField(`**want to add me to your server?**`, 'add me [here](https://top.gg/bot/768468297552756797)')
+  .addField(`**want to add me to your server?**`, 'add me [here](https://discordbotlist.com/bots/sefy)')
   .addField(`**don't forget to join those server as well to make friends or just want to get support for me:**`, '[Sefiria](https://discord.gg/D6rWrvS), where everything started, all of my commands were built from this server\'s community. *(join now if you play AstolfoBot)*\n\n[my support server](https://discord.gg/kJRAjMyEkY), if you want to get some support or contribute!')
   return message.channel.send(embed);
 };
