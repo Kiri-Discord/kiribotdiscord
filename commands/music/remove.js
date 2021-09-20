4c363f50c7dd5848ed74798cb0939f973a87cb73
ryzenix <61915235+ryzenix@users.noreply.github.com>
2021-09-20 16:16:16 +0700

    Update remove.js

--- commands/music/remove.js
+++ commands/music/remove.js
@@ -4,28 +4,28 @@ const pattern = /^[0-9]{1,2}(\s*,\s*[0-9]{1,2})*$/;
 
 exports.run = async(client, message, args, prefix) => {
     const queue = client.queue.get(message.guild.id);
-    if (!queue) return message.channel.send({ embed: { color: "f3f3f3", description: `:x: there isn't any ongoing music queue` } });
-    if (!canModifyQueue(message.member)) return message.channel.send({ embed: { color: "f3f3f3", description: `you have to be in ${queue.channel} to do this command :(` } });
-    if (!queue.songs.length) return message.channel.send({ embed: { color: "f3f3f3", description: `there isn't any song left in the queue :pensive:` } });
-    if (!args.length) return message.channel.send({ embed: { color: "f3f3f3", description: `you must to tell me what song you want to remove! use \`${prefix}help remove\` to learn more :wink:` } });
+    if (!queue) return message.channel.send({ embeds: [{ color: "f3f3f3", description: `:x: there isn't any ongoing music queue` }] });
+    if (!canModifyQueue(message.member)) return message.channel.send({ embeds: [{ color: "f3f3f3", description: `you have to be in ${queue.channel} to do this command :(` }] });
+    if (!queue.songs.length) return message.channel.send({ embeds: [{ color: "f3f3f3", description: `there isn't any song left in the queue :pensive:` }] });
+    if (!args.length) return message.channel.send({ embeds: [{ color: "f3f3f3", description: `you must to tell me what song you want to remove! use \`${prefix}help remove\` to learn more :wink:` }] });
 
     const arguments = args.join(" ");
     let removed = [];
     const songs = arguments.split(",").map((str) => str.trim());
 
     if (pattern.test(arguments)) {
-        if (songs.some(x => x > queue.songs.length)) return message.channel.send({ embed: { color: "f3f3f3", description: `that is not a number or a correct position in the queue :pensive:` } });
+        if (songs.some(x => x > queue.songs.length)) return message.channel.send({ embeds: [{ color: "f3f3f3", description: `that is not a number or a correct position in the queue :pensive:` }] });
         queue.songs = queue.songs.filter((item, index) => {
             if (songs.find((songIndex) => songIndex - 1 === index)) removed.push(item);
             else return true;
         });
-        if (queue.textChannel.id !== message.channel.id) message.channel.send({ embed: { color: "f3f3f3", description: `❌ you removed **${removed.map((song) => song.title).join("\n")}** from the queue` } });
-        return queue.textChannel.send({ embed: { color: "f3f3f3", description: `${message.author} removed **${removed.map((song) => song.info.title).join("\n")}** from the queue ❌` } })
+        if (queue.textChannel.id !== message.channel.id) message.channel.send({ embeds: [{ color: "f3f3f3", description: `❌ you removed **${removed.map((song) => song.info.title).join("\n")}** from the queue` }] });
+        return queue.textChannel.send({ embeds: [{ color: "f3f3f3", description: `${message.author} removed **${removed.map((song) => song.info.title).join("\n")}** from the queue ❌` }] })
     } else if (!isNaN(args[0]) && songs.length <= 1 && args[0] > 1 && args[0] <= queue.songs.length) {
-        if (queue.textChannel.id !== message.channel.id) message.channel.send({ embed: { color: "f3f3f3", description: `❌ you removed **${queue.songs.splice(args[0] - 1, 1)[0].info.title}** from the queue ❌` } });
-        return queue.textChannel.send({ embed: { color: "f3f3f3", description: `${message.author} removed **${queue.songs.splice(args[0] - 1, 1)[0].title}** from the queue ❌` } });
+        if (queue.textChannel.id !== message.channel.id) message.channel.send({ embeds: [{ color: "f3f3f3", description: `❌ you removed **${queue.songs.splice(args[0] - 1, 1)[0].info.title}** from the queue ❌` }] });
+        return queue.textChannel.send({ embeds: [{ color: "f3f3f3", description: `${message.author} removed **${queue.songs.splice(args[0] - 1, 1)[0].info.title}** from the queue ❌` }] });
     } else {
-        return message.channel.send({ embed: { color: "f3f3f3", description: `wrong usage! use \`${prefix}help remove\` to learn more :wink:` } });
+        return message.channel.send({ embeds: [{ color: "f3f3f3", description: `wrong usage! use \`${prefix}help remove\` to learn more :wink:` }] });
     };
 };
 
