8147c7cc50330bba1ba63a6326cc3b2cf3f029d5
ryzenix <61915235+ryzenix@users.noreply.github.com>
2021-09-20 17:22:21 +0700

    fix a typo at voice event

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -11,7 +11,7 @@ module.exports = async(client, oldState, newState) => {
             await client.lavacordManager.leave(queue.textChannel.guild.id);
             return client.queue.delete(queue.textChannel.guild.id);
         };
-        const playerListening = [...queue.channel.members.value()];
+        const playerListening = [...queue.channel.members.values()];
         let listening = playerListening.filter(x => !x.user.bot).length;
         if (listening >= 1) return;
 
