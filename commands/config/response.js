1aeafa0d6a2ae88b10632730b27b25906a3ae8f0
ryzenix <61915235+ryzenix@users.noreply.github.com>
2021-09-27 06:48:34 +0700

    added a prompt when user didnt include anything (#173)

--- commands/config/response.js
+++ commands/config/response.js
@@ -1,7 +1,8 @@
 const types = ["cute", "natural"]
 
 exports.run = async(client, message, args) => {
-    const type = args[0].toLowerCase();
+    if (!args[0]) return message.reply({ embeds: [{ color: "#bee7f7", description: `you didn't include a response type :pensive: all the avaliable response are \`${types.join(', ')}\`` }] });
+    const type = args[0].trim().toLowerCase();
     if (!types.includes(type)) return message.reply({ embeds: [{ color: "#bee7f7", description: `\`${args[0]}\` isn't a valid response mode :pensive: all the avaliable response are \`${types.join(', ')}\`` }] })
     const db = client.guildsStorage.get(message.guild.id);
     db.responseType = args[0];
