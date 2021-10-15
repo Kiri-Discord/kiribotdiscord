07e73c4d9c9e0fef40bf8d5e3176983b3dd9654d
ryzenix <61915235+ryzenix@users.noreply.github.com>
2021-10-15 09:00:15 +0700

    fix(chess): fix problems with save data

--- model/game.js
+++ model/game.js
@@ -1,21 +1,21 @@
 const mongoose = require('mongoose');
 
 const reqString = {
-  type: String,
-  required: true,
+    type: String,
+    required: true,
 }
 
 const gameSchema = mongoose.Schema({
-  guildId: reqString,
-  userId: reqString,
-  storage: {
-    type: Object,
-    default: Object
-  },
+    guildId: reqString,
+    userId: reqString,
+    storage: {
+        type: Object,
+        default: Object
+    },
 });
 
 module.exports = mongoose.model(
-  'gameStorage',
-  gameSchema,
-  'gameStorage'
+    'gameStorage',
+    gameSchema,
+    'gameStorage'
 )
