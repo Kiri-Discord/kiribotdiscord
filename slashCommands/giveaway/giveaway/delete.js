fac0bfa97f09766325d079068c4d9680c9ba13ae
ryzenix <61915235+ryzenix@users.noreply.github.com>
2021-10-21 18:54:22 +0700

    migration: update giveaway module

---
+++ slashCommands/giveaway/giveaway/delete.js
@@ -0,0 +1,54 @@
+const { Permissions } = require('discord.js');
+
+exports.run = async(client, interaction) => {
+    const messageId = interaction.options.getString('message-id');
+    if (!messageId) {
+        const onChannel = client.giveaways.giveaways
+            .filter(g => g.channelId === interaction.channel.id);
+        if (!onChannel || !onChannel.length) return interaction.reply({
+            embeds: [{
+                color: 'RED',
+                description: `:boom: there isn't any ongoing giveaway on this channel :pensive:\nuse \`/giveaway list\` to get the remaining giveaways on this server, then run this command again with the message ID of that giveaway by \`/giveaway delete <message ID>\` :grin:`
+            }],
+            ephemeral: true
+        })
+        const matches = onChannel.sort((a, b) => b.startAt - a.startAt);
+        const firstGiveaway = matches[0];
+        if (firstGiveaway.extraData.hostedByID !== interaction.user.id && !interaction.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) return interaction.reply({
+            embeds: [{
+                description: 'that isn\'t your giveaway, or you don\'t have the \`ADMINISTRATOR\` permission to take full control over that giveaway :pensive:',
+                color: 'RED'
+            }]
+        });
+        await interaction.deferReply();
+        await client.giveaways.delete(firstGiveaway.messageId, true);
+        return interaction.editReply({ embeds: [{ description: `✅ the giveaway ${firstGiveaway.prize} was deleted :pensive:`, color: 'RED' }] })
+    } else {
+        const onServer = client.giveaways.giveaways.filter(g => g.guildId === interaction.guild.id);
+        if (!onServer || !onServer.length) return interaction.reply({
+            embeds: [{
+                color: 'RED',
+                description: `:boom: there isn't any ongoing giveaway on the server :pensive:`
+            }],
+            ephemeral: true
+        });
+        const giveaway = onServer.find(g => g.messageId === messageId);
+        if (!giveaway) return interaction.reply({
+            embeds: [{
+                description: "there isn't any giveaway with that message ID :pensive:",
+                color: 'RED'
+            }],
+            ephemeral: true
+        })
+        if (giveaway.extraData.hostedByID !== interaction.user.id && !interaction.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) return interaction.reply({
+            embeds: [{
+                description: 'that isn\'t your giveaway, or you don\'t have the \`ADMINISTRATOR\` permission to take full control over that giveaway :pensive:',
+                color: 'RED'
+            }],
+            ephemeral: true
+        });
+        await interaction.deferReply();
+        await client.giveaways.delete(giveaway.messageId, true);
+        return interaction.reply({ embeds: [{ description: `the giveaway ${giveaway.prize} was deleted :pensive:`, color: 'RED' }] })
+    };
+};
---
+++ slashCommands/giveaway/giveaway/list.js
@@ -0,0 +1,27 @@
+const { MessageEmbed } = require('discord.js');
+const { embedURL } = require('../../../util/util');
+
+exports.run = async(client, interaction) => {
+    const onServer = client.giveaways.giveaways
+        .filter(g => g.guildId === interaction.guild.id);
+    if (!onServer || !onServer.length) return interaction.reply({
+        embeds: [{
+            description: ":boom: sorry, there isn't any existing giveaway for your server :pensive:",
+            color: 'RED'
+        }],
+        ephemeral: true
+    });
+    const all = onServer.sort((a, b) => b.startAt - a.startAt);
+    let list = [];
+    all.forEach((giveaway) => list.push(!giveaway.ended ?
+        `${giveaway.hostedBy} • **${giveaway.prize}** | ends in: <t:${giveaway.endAt}:R> | ${embedURL('Jump to giveaway', giveaway.messageURL)} (message ID: \`${giveaway.messageId})\`` :
+        `**ENDED** ${giveaway.hostedBy} • **${giveaway.prize}** | ${embedURL('Jump to giveaway', giveaway.messageURL)} (message ID: \`${giveaway.messageId}\``));
+
+    const embed = new MessageEmbed()
+        .setDescription(list.join('\n'))
+        .setAuthor(`giveaway list for ${interaction.guild.name}`, interaction.guild.iconURL({ size: 4096, dynamic: true }))
+        .setTimestamp()
+        .setFooter(`${onServer.filter(g => !g.ended).length} ongoing giveaway`)
+
+    return interaction.reply({ embeds: [embed] });
+};
