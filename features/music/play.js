8864df446d2e24c869590bd8aca8b960d858d444
ryzenix <61915235+ryzenix@users.noreply.github.com>
2021-11-07 11:03:12 +0700

    Update play.js

--- features/music/play.js
+++ features/music/play.js
@@ -51,7 +51,7 @@ module.exports = class Queue {
         };
         this.client.queue.delete(this.guildId);
         if (this.player) {
-            this.player.stop();
+            if (reason !== 'disconnected') this.player.stop();
             this.client.lavacordManager.players.delete(this.guildId);
         };
         await Guild.findOneAndUpdate({
