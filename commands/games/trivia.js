b6b5b2da7e0034c6ce8619ac53e40def863a03ac
ryzenix <61915235+ryzenix@users.noreply.github.com>
2022-01-21 15:24:28 +0700

    fix trivia bug

--- commands/games/trivia.js
+++ commands/games/trivia.js
@@ -20,7 +20,8 @@ exports.run = async(client, message, args) => {
         difficulty = difficulties[Math.floor(Math.random() * difficulties.length)]
     };
     client.games.set(message.channel.id, { prompt: `please wait until **${message.author.username}** is finished with their game first ${duhEmoji}` });
-    const { body } = await request
+    try {
+        const { body } = await request
         .get('https://opentdb.com/api.php')
         .query({
             amount: 1,
@@ -28,89 +29,92 @@ exports.run = async(client, message, args) => {
             encode: 'url3986',
             difficulty
         });
-    if (!body.results) return message.channel.send(`i couldn't fetch any trivia question! try again later ${sadEmoji}`);
-    const answers = body.results[0].incorrect_answers.map(answer => decodeURIComponent(answer));
-    const correct = decodeURIComponent(body.results[0].correct_answer);
-    answers.push(correct);
-    const shuffled = shuffle(answers);
-    const questionEmbed = new MessageEmbed()
-        .setColor('#7DBBEB')
-        .setFooter({text: `this question will be timed out in 15 seconds`})
-        .setTitle('Trivia')
-        .addField('Topic', `\`${decodeURIComponent(body.results[0].category)}\``, true)
-        .addField('Question', `**${decodeURIComponent(body.results[0].question)}**`, true)
-        .addField('Answers', shuffled.map((answer, i) => `**${choices[i]}** \`${answer}\``).join('\n'))
-        .setColor('#bee7f7')
-    const row = new MessageActionRow()
-        .addComponents(
-            new MessageButton()
-            .setCustomId('A')
-            .setLabel('A')
-            .setStyle('SECONDARY'),
-            new MessageButton()
-            .setCustomId('B')
-            .setLabel('B')
-            .setStyle('SECONDARY'),
-            new MessageButton()
-            .setCustomId('C')
-            .setLabel('C')
-            .setStyle('SECONDARY'),
-            new MessageButton()
-            .setCustomId('D')
-            .setLabel('D')
-            .setStyle('SECONDARY')
-        );
-    const msg = await message.channel.send({ embeds: [questionEmbed], components: [row] });
-    const collector = msg.createMessageComponentCollector({
-        componentType: 'BUTTON',
-        time: 15000,
-        max: 1
-    });
-    let winner;
-
-    collector.on('collect', async res => {
-        await res.deferReply();
-        const answerEmbed = new MessageEmbed()
+        if (!body.results) return message.channel.send(`i couldn't fetch any trivia question! try again later ${sadEmoji}`);
+        const answers = body.results[0].incorrect_answers.map(answer => decodeURIComponent(answer));
+        const correct = decodeURIComponent(body.results[0].correct_answer);
+        answers.push(correct);
+        const shuffled = shuffle(answers);
+        const questionEmbed = new MessageEmbed()
+            .setColor('#7DBBEB')
+            .setFooter({text: `this question will be timed out in 15 seconds`})
+            .setTitle('Trivia')
+            .addField('Topic', `\`${decodeURIComponent(body.results[0].category)}\``, true)
+            .addField('Question', `**${decodeURIComponent(body.results[0].question)}**`, true)
+            .addField('Answers', shuffled.map((answer, i) => `**${choices[i]}** \`${answer}\``).join('\n'))
             .setColor('#bee7f7')
-        if (shuffled[choices.indexOf(res.customId)] === correct) {
-            winner = res.user;
-            let amount = getRandomInt(20, 40);
-            const storageAfter = await client.money.findOneAndUpdate({
-                guildId: message.guild.id,
-                userId: winner.id
-            }, {
-                guildId: message.guild.id,
-                userId: winner.id,
-                $inc: {
-                    balance: amount,
-                },
-            }, {
-                upsert: true,
-                new: true,
-            });
-            answerEmbed
-                .setFooter({text: `your current balance: ${storageAfter.balance}`})
-                .setTitle(`you gave the correct answer, ${winner.username}`)
-                .setDescription(`⏣ **${amount}** token was placed in your wallet :boom:`)
-            res.editReply({ embeds: [answerEmbed] });
-        } else {
-            answerEmbed
-                .setFooter({text: 'better luck next time!'})
-                .setTitle(`sorry, you provided a wrong answer!`)
-                .setDescription(`the correct answer was: \`${correct}\` ${sadEmoji}`)
-            res.editReply({ embeds: [answerEmbed] });
-        };
-    });
-    collector.on('end', (collected) => {
-        client.games.delete(message.channel.id);
-        if (collected.size && shuffled[choices.indexOf(collected.first().customId)] !== correct) row.components.find(component => component.customId === collected.first().customId).style = 'DANGER';
-        // row.components.find(component => shuffled[choices.indexOf(component.customId)] === correct).style = 'SUCCESS';
-        row.components.forEach(component => {
-            if (shuffled[choices.indexOf(component.customId)] === correct) component.style = 'SUCCESS';
-            component.setDisabled(true)
+        const row = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                .setCustomId('A')
+                .setLabel('A')
+                .setStyle('SECONDARY'),
+                new MessageButton()
+                .setCustomId('B')
+                .setLabel('B')
+                .setStyle('SECONDARY'),
+                new MessageButton()
+                .setCustomId('C')
+                .setLabel('C')
+                .setStyle('SECONDARY'),
+                new MessageButton()
+                .setCustomId('D')
+                .setLabel('D')
+                .setStyle('SECONDARY')
+            );
+        const msg = await message.channel.send({ embeds: [questionEmbed], components: [row] });
+        const collector = msg.createMessageComponentCollector({
+            componentType: 'BUTTON',
+            time: 15000,
+            max: 1
         });
-        return msg.edit({ components: [row] });
-    });
+        let winner;
+
+        collector.on('collect', async res => {
+            await res.deferReply();
+            const answerEmbed = new MessageEmbed()
+                .setColor('#bee7f7')
+            if (shuffled[choices.indexOf(res.customId)] === correct) {
+                winner = res.user;
+                let amount = getRandomInt(20, 40);
+                const storageAfter = await client.money.findOneAndUpdate({
+                    guildId: message.guild.id,
+                    userId: winner.id
+                }, {
+                    guildId: message.guild.id,
+                    userId: winner.id,
+                    $inc: {
+                        balance: amount,
+                    },
+                }, {
+                    upsert: true,
+                    new: true,
+                });
+                answerEmbed
+                    .setFooter({text: `your current balance: ${storageAfter.balance}`})
+                    .setTitle(`you gave the correct answer, ${winner.username}`)
+                    .setDescription(`⏣ **${amount}** token was placed in your wallet :boom:`)
+                res.editReply({ embeds: [answerEmbed] });
+            } else {
+                answerEmbed
+                    .setFooter({text: 'better luck next time!'})
+                    .setTitle(`sorry, you provided a wrong answer!`)
+                    .setDescription(`the correct answer was: \`${correct}\` ${sadEmoji}`)
+                res.editReply({ embeds: [answerEmbed] });
+            };
+        });
+        collector.on('end', (collected) => {
+            client.games.delete(message.channel.id);
+            if (collected.size && shuffled[choices.indexOf(collected.first().customId)] !== correct) row.components.find(component => component.customId === collected.first().customId).style = 'DANGER';
+            // row.components.find(component => shuffled[choices.indexOf(component.customId)] === correct).style = 'SUCCESS';
+            row.components.forEach(component => {
+                if (shuffled[choices.indexOf(component.customId)] === correct) component.style = 'SUCCESS';
+                component.setDisabled(true)
+            });
+            return msg.edit({ components: [row] });
+        });
+    } catch {
+        client.games.delete(message.channel.id);
+    };
 };
 
 function getRandomInt(min, max) {
--- slashCommands/games/trivia.js
+++ slashCommands/games/trivia.js
@@ -13,97 +13,101 @@ exports.run = async(client, interaction, args) => {
     let difficulty = interaction.options.getString('difficulty') || difficulties[Math.floor(Math.random() * difficulties.length)];
 
     client.games.set(interaction.channel.id, { prompt: `please wait until **${interaction.user.username}** is finished with their game first ${duhEmoji}` });
-    await interaction.deferReply();
-    const { body } = await request
-        .get('https://opentdb.com/api.php')
-        .query({
-            amount: 1,
-            type: 'multiple',
-            encode: 'url3986',
-            difficulty
-        });
-    if (!body.results) return interaction.editReply({ content: `i couldn't fetch any trivia question! try again later ${sadEmoji}` });
-    const answers = body.results[0].incorrect_answers.map(answer => decodeURIComponent(answer));
-    const correct = decodeURIComponent(body.results[0].correct_answer);
-    answers.push(correct);
-    const shuffled = shuffle(answers);
-    const questionEmbed = new MessageEmbed()
-        .setColor('#7DBBEB')
-        .setFooter({text: `this question will be timed out in 15 seconds`})
-        .setTitle('Trivia')
-        .addField('Topic', `\`${decodeURIComponent(body.results[0].category)}\``, true)
-        .addField('Question', `**${decodeURIComponent(body.results[0].question)}**`, true)
-        .addField('Answers', shuffled.map((answer, i) => `**${choices[i]}** \`${answer}\``).join('\n'))
-        .setColor('#bee7f7')
-    const row = new MessageActionRow()
-        .addComponents(
-            new MessageButton()
-            .setCustomId('A')
-            .setLabel('A')
-            .setStyle('SECONDARY'),
-            new MessageButton()
-            .setCustomId('B')
-            .setLabel('B')
-            .setStyle('SECONDARY'),
-            new MessageButton()
-            .setCustomId('C')
-            .setLabel('C')
-            .setStyle('SECONDARY'),
-            new MessageButton()
-            .setCustomId('D')
-            .setLabel('D')
-            .setStyle('SECONDARY')
-        );
-    const msg = await interaction.editReply({ embeds: [questionEmbed], components: [row], fetchReply: true });
-    const collector = msg.createMessageComponentCollector({
-        componentType: 'BUTTON',
-        time: 15000,
-        max: 1
-    });
-    let winner;
-
-    collector.on('collect', async res => {
-        await res.deferReply();
-        const answerEmbed = new MessageEmbed()
-            .setColor('#bee7f7')
-        if (shuffled[choices.indexOf(res.customId)] === correct) {
-            winner = res.user;
-            let amount = getRandomInt(20, 40);
-            const storageAfter = await client.money.findOneAndUpdate({
-                guildId: interaction.guild.id,
-                userId: winner.id
-            }, {
-                guildId: interaction.guild.id,
-                userId: winner.id,
-                $inc: {
-                    balance: amount,
-                },
-            }, {
-                upsert: true,
-                new: true,
+    try {
+        await interaction.deferReply();
+        const { body } = await request
+            .get('https://opentdb.com/api.php')
+            .query({
+                amount: 1,
+                type: 'multiple',
+                encode: 'url3986',
+                difficulty
             });
-            answerEmbed
-                .setFooter({text: `your current balance: ${storageAfter.balance}`})
-                .setTitle(`you gave the correct answer, ${winner.username}`)
-                .setDescription(`⏣ **${amount}** token was placed in your wallet :boom:`)
-            res.editReply({ embeds: [answerEmbed] });
-        } else {
-            answerEmbed
-                .setFooter({text: 'better luck next time!'})
-                .setTitle(`sorry, you provided a wrong answer!`)
-                .setDescription(`the correct answer was: \`${correct}\` ${sadEmoji}`)
-            res.editReply({ embeds: [answerEmbed] });
-        };
-    });
-    collector.on('end', (collected) => {
-        client.games.delete(interaction.channel.id);
-        if (collected.size && shuffled[choices.indexOf(collected.first().customId)] !== correct) row.components.find(component => component.customId === collected.first().customId).style = 'DANGER';
-        row.components.forEach(component => {
-            if (shuffled[choices.indexOf(component.customId)] === correct) component.style = 'SUCCESS';
-            component.setDisabled(true)
+        if (!body.results) return interaction.editReply({ content: `i couldn't fetch any trivia question! try again later ${sadEmoji}` });
+        const answers = body.results[0].incorrect_answers.map(answer => decodeURIComponent(answer));
+        const correct = decodeURIComponent(body.results[0].correct_answer);
+        answers.push(correct);
+        const shuffled = shuffle(answers);
+        const questionEmbed = new MessageEmbed()
+            .setColor('#7DBBEB')
+            .setFooter({text: `this question will be timed out in 15 seconds`})
+            .setTitle('Trivia')
+            .addField('Topic', `\`${decodeURIComponent(body.results[0].category)}\``, true)
+            .addField('Question', `**${decodeURIComponent(body.results[0].question)}**`, true)
+            .addField('Answers', shuffled.map((answer, i) => `**${choices[i]}** \`${answer}\``).join('\n'))
+            .setColor('#bee7f7')
+        const row = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                .setCustomId('A')
+                .setLabel('A')
+                .setStyle('SECONDARY'),
+                new MessageButton()
+                .setCustomId('B')
+                .setLabel('B')
+                .setStyle('SECONDARY'),
+                new MessageButton()
+                .setCustomId('C')
+                .setLabel('C')
+                .setStyle('SECONDARY'),
+                new MessageButton()
+                .setCustomId('D')
+                .setLabel('D')
+                .setStyle('SECONDARY')
+            );
+        const msg = await interaction.editReply({ embeds: [questionEmbed], components: [row], fetchReply: true });
+        const collector = msg.createMessageComponentCollector({
+            componentType: 'BUTTON',
+            time: 15000,
+            max: 1
         });
-        return msg.edit({ components: [row] });
-    });
+        let winner;
+    
+        collector.on('collect', async res => {
+            await res.deferReply();
+            const answerEmbed = new MessageEmbed()
+                .setColor('#bee7f7')
+            if (shuffled[choices.indexOf(res.customId)] === correct) {
+                winner = res.user;
+                let amount = getRandomInt(20, 40);
+                const storageAfter = await client.money.findOneAndUpdate({
+                    guildId: interaction.guild.id,
+                    userId: winner.id
+                }, {
+                    guildId: interaction.guild.id,
+                    userId: winner.id,
+                    $inc: {
+                        balance: amount,
+                    },
+                }, {
+                    upsert: true,
+                    new: true,
+                });
+                answerEmbed
+                    .setFooter({text: `your current balance: ${storageAfter.balance}`})
+                    .setTitle(`you gave the correct answer, ${winner.username}`)
+                    .setDescription(`⏣ **${amount}** token was placed in your wallet :boom:`)
+                res.editReply({ embeds: [answerEmbed] });
+            } else {
+                answerEmbed
+                    .setFooter({text: 'better luck next time!'})
+                    .setTitle(`sorry, you provided a wrong answer!`)
+                    .setDescription(`the correct answer was: \`${correct}\` ${sadEmoji}`)
+                res.editReply({ embeds: [answerEmbed] });
+            };
+        });
+        collector.on('end', (collected) => {
+            client.games.delete(interaction.channel.id);
+            if (collected.size && shuffled[choices.indexOf(collected.first().customId)] !== correct) row.components.find(component => component.customId === collected.first().customId).style = 'DANGER';
+            row.components.forEach(component => {
+                if (shuffled[choices.indexOf(component.customId)] === correct) component.style = 'SUCCESS';
+                component.setDisabled(true)
+            });
+            return msg.edit({ components: [row] });
+        });
+    } catch {
+        client.games.delete(interaction.channel.id);
+    };
 };
 
 function getRandomInt(min, max) {
