7338384b5b6db22c293d663b647651c094db2a83
ryzenix <61915235+ryzenix@users.noreply.github.com>
2022-05-05 16:28:48 +0700

    Update autoplay.js

--- slashCommands/music/music/autoplay.js
+++ slashCommands/music/music/autoplay.js
@@ -4,7 +4,7 @@ exports.run = async(client, interaction) => {
     const queue = client.queue.get(interaction.guild.id);
     if (!queue) return interaction.reply({ embeds: [{ color: "#bee7f7", description: `:x: there isn't any ongoing music queue` }], ephemeral: true });
     if (!canModifyQueue(interaction.member)) return interaction.reply({ embeds: [{ color: "#bee7f7", description: `you have to be in ${queue.channel} to do this command :(` }], ephemeral: true });
-    if (queue.karaoke.isEnabled) return interaction.reply({ embeds: [{ color: "#bee7f7", description: `autoplay is not possible when scrolling lyrics is on :pensive: you can turn it off by \`${prefix}scrolling-lyrics off\`` }], ephemeral: true });
+    if (queue.karaoke.isEnabled) return interaction.reply({ embeds: [{ color: "#bee7f7", description: `autoplay is not possible when scrolling lyrics is on :pensive: you can turn it off by \`/scrolling-lyrics off\`` }], ephemeral: true });
     queue.autoPlay = !queue.autoPlay;
     const autoEmoji = client.customEmojis.get('autoplay') ? client.customEmojis.get('autoplay').toString() : '🔁'
     return interaction.reply({ embeds: [{ color: "#bee7f7", description: `${autoEmoji} autoplay has been turned ${queue.autoPlay ? "on" : "off"} for the current queue! ${queue.autoPlay ? "i will start enqueuing new song when the queue is empty!" : ""}` }] })
