0fa97d1f25af5319afed8d0e063a659315ed1730
ryzenix <61915235+ryzenix@users.noreply.github.com>
2022-06-18 11:57:24 +0700

    Revert "jesus"
    This reverts commit 871a799053c29d7be99389ff3581b1b65e0585d4.

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,30 +1,16 @@
-const { formatDuration } = require('../../util/musicutil');
-const { MessageEmbed } = require("discord.js");
-
 exports.run = async(client, message, args) => {
     const pingMessage = await message.channel.send(`almost there...`);
     const ping = pingMessage.createdTimestamp - message.createdTimestamp;
-
-    const embed = new MessageEmbed()
-    .setDescription(`
-    some additional statuses:
-    > • Discord WebSocket ping: \`${client.ws.ping}\`ms
-    > • shard uptime: \`${formatDuration(client.uptime)}\`
-    > • this shard has \`${client.guilds.cache.size}\` guilds
-    > • currently on shard \`${message.channel.type === "DM" ? 0 : message.guild.shardId}\`
-    `)
-    return pingMessage.edit({ content: `pong! took me \`${ping}\`ms!`, embeds: [embed] });
+    return pingMessage.edit(`:ping_pong: pong! took me ${ping}ms, and Discord ${Math.round(client.ws.ping)}ms`);
 };
-
 exports.help = {
     name: "ping",
     description: "ping me and check if i'm actually alive or not",
-    usage: [`ping`],
-    example: [`ping`]
+    usage: ["ping"],
+    example: ["ping"]
 };
 
 exports.conf = {
-    channelPerms: ["EMBED_LINKS"],
     aliases: [],
     cooldown: 2,
 };
